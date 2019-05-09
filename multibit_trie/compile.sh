102d5ae548ae08f855adfbcad6c6b41d9d3de6cf
Knium <hi.im.knium@gmail.com>
2019-05-09 15:56:30 +0900

    Try to build on multiple Operating Systems.

---
+++ multibit_trie/compile.sh
@@ -0,0 +1,14 @@
+# Following targets are not supported by cross.
+# - i686-apple-darwin
+# - x86_64-apple-darwin
+# - i686-pc-windows-msvc
+# - x86_64-pc-windows-msvc
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    cross build --release --target i686-pc-windows-gnu
+    cross build --release --target x86_64-pc-windows-gnu
+    cross build --release --target x86_64-unknown-linux-gnu
+    cross build --release --target i686-unknown-linux-gnu
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    cargo build --release
+fi
