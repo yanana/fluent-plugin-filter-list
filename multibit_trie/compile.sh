e6afdc077b11a3b0cce008605ded32e69b2471f0
Knium <hi.im.knium@gmail.com>
2019-05-13 15:45:48 +0900

    Skip cross-compile.

--- multibit_trie/compile.sh
+++ multibit_trie/compile.sh
@@ -4,11 +4,17 @@
 # - i686-pc-windows-msvc
 # - x86_64-pc-windows-msvc
 
+cargo build --release
+
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    cross build --release --target x86_64-unknown-linux-gnu
-    cross build --release --target i686-unknown-linux-gnu
-    cross build --release --target i686-pc-windows-gnu
-    cross build --release --target x86_64-pc-windows-gnu
+    # cross build --release --target x86_64-unknown-linux-gnu
+    # cross build --release --target i686-unknown-linux-gnu
+    # cross build --release --target i686-pc-windows-gnu
+    # cross build --release --target x86_64-pc-windows-gnu
+
+    cp ./target/release/libmultibit_trie.d ../lib/libmultibit_trie.dylib
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    cross build --release --target x86_64-apple-darwin
+    # cross build --release --target x86_64-apple-darwin
+
+    cp ./target/release/libmultibit_trie.dylib ../lib/
 fi
