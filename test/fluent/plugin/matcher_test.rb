d9685227ead389839ee1df675a840aa23a0d82a6
Shun Yanaura <metroplexity@gmail.com>
2021-09-14 19:58:53 +0900

    fix: recreate AC automaton

--- test/fluent/plugin/matcher_test.rb
+++ test/fluent/plugin/matcher_test.rb
@@ -1,44 +1,17 @@
 require 'test_helper'
 require 'matcher'
 
-class ACMatcherTest < Minitest::Test # rubocop:disable Metrics/ClassLength
+class ACMatcherTest < Minitest::Test
   include Matchers
 
   def test_that_tree_without_leaves_is_built_given_nil_is_passed
-    acmatcher = ACMatcher.new(nil)
-    assert_equal(acmatcher.trie.root.children, {})
+    ac = ACAutomaton.new(nil)
+    assert(ac.nodes[0].goto.empty?)
   end
 
   def test_that_tree_without_leaves_is_built_given_empty_array_is_passed
-    acmatcher = ACMatcher.new([])
-    assert_equal(acmatcher.trie.root.children, {})
-  end
-
-  def test_that_internal_nodes_have_only_one_child_and_leaf_node_has_output_given_singleton_array_is_passed
-    acmatcher = ACMatcher.new(['hoge'])
-    current_node = acmatcher.trie.root
-    while current_node.children.size == 1
-      assert(current_node.children.size == 1)
-      current_node = current_node.children.values[0]
-    end
-    assert(current_node.output.include?('hoge'))
-  end
-
-  def test_that_output_consists_of_elements_of_input
-    kws = %w[hoge bar bar2 abhc]
-    acmatcher = ACMatcher.new(kws)
-
-    @outputs = []
-
-    detect_output_of_child = lambda do |node|
-      node.children.each_value do |child_node|
-        @outputs.push(child_node.output) unless child_node.output.nil?
-        detect_output_of_child.call(child_node)
-      end
-    end
-
-    detect_output_of_child.call(acmatcher.trie.root)
-    assert((@outputs - kws).empty?)
+    acm = ACAutomaton.new([])
+    assert(acm.nodes[0].goto.empty?)
   end
 
   def test_matches_when_a_pattern_is_in_the_middle_of_text
@@ -76,27 +49,27 @@ class ACMatcherTest < Minitest::Test # rubocop:disable Metrics/ClassLength
     acmatcher = ACMatcher.new(kw)
     assert(acmatcher.matches?("abc#{k1}"))
     assert(acmatcher.matches?("abc#{k2}"))
-    assert(!acmatcher.matches?('abcabc'))
+    refute(acmatcher.matches?('abcabc'))
   end
 
   def test_that_blank_pattern_does_not_match
     acmatcher = ACMatcher.new([''])
-    assert(!acmatcher.matches?('foo'))
+    refute(acmatcher.matches?('foo'))
   end
 
   def test_nil_pattern_results_to_no_characters_detected
     acmatcher = ACMatcher.new(nil)
-    assert(!acmatcher.matches?('foo'))
+    refute(acmatcher.matches?('foo'))
   end
 
   def test_nil_text_never_matches
     matcher = ACMatcher.new(['xyz'])
-    assert !matcher.matches?(nil)
+    refute matcher.matches?(nil)
   end
 
   def test_blank_text_never_matches
     matcher = ACMatcher.new(['foo'])
-    assert(!matcher.matches?(''))
+    refute(matcher.matches?(''))
   end
 
   def test_text_other_than_string_is_converted_to_string_before_comparison
@@ -112,19 +85,74 @@ class ACMatcherTest < Minitest::Test # rubocop:disable Metrics/ClassLength
     assert(m.matches?('axy'))
     assert(m.matches?('bxy'))
     assert(m.matches?('xyz'))
-    assert(!m.matches?('yz'))
-    assert(!m.matches?('ax'))
-    assert(!m.matches?('axc'))
-    assert(!m.matches?('x'))
-    assert(!m.matches?('y'))
+    refute(m.matches?('yz'))
+    refute(m.matches?('ax'))
+    refute(m.matches?('axc'))
+    refute(m.matches?('x'))
+    refute(m.matches?('y'))
   end
 
   def test_failure_link
     m = ACMatcher.new(%w[xyz abxyz])
     assert(m.matches?('xyz'))
     assert(m.matches?('axyz'))
-    assert(!m.matches?('abxee'))
+    refute(m.matches?('abxee'))
     assert(m.matches?('abxeexyz'))
+  end
+
+  def test_find_nothing_matched
+    p = %w[she]
+    text = 'bad'
+    m = ACMatcher.new(p)
+    assert_equal([], m.find(text))
+  end
+
+  def test_find_exact_match
+    p = %w[she]
+    text = 'she'
+    m = ACMatcher.new(p)
+    assert_equal(%w[she], m.find(text))
+  end
+
+  def test_find_partial_match
+    p = %w[she]
+    text = 'sh'
+    m = ACMatcher.new(p)
+    assert_equal([], m.find(text))
+  end
+
+  def test_build
+    p = %w[hers his she he]
+    ac = ACAutomaton.new(p)
+    nodes = [
+      ACAutomaton::Node.new(id: 0, goto: { 'h' => 1, 's' => 7 }, failure: 0, out: []),
+      ACAutomaton::Node.new(id: 1, goto: { 'e' => 2, 'i' => 5 }, failure: 0, out: []),
+      ACAutomaton::Node.new(id: 2, goto: { 'r' => 3 }, failure: 0, out: [3]),
+      ACAutomaton::Node.new(id: 3, goto: { 's' => 4 }, failure: 0, out: []),
+      ACAutomaton::Node.new(id: 4, goto: {}, failure: 7, out: [0]),
+      ACAutomaton::Node.new(id: 5, goto: { 's' => 6 }, failure: 0, out: []),
+      ACAutomaton::Node.new(id: 6, goto: {}, failure: 7, out: [1]),
+      ACAutomaton::Node.new(id: 7, goto: { 'h' => 8 }, failure: 0, out: []),
+      ACAutomaton::Node.new(id: 8, goto: { 'e' => 9 }, failure: 1, out: []),
+      ACAutomaton::Node.new(id: 9, goto: {}, failure: 2, out: [2, 3])
+    ]
+    nodes.each do |node|
+      n = ac.nodes[node.id]
+      assert_equal(node, n)
+    end
+  end
+
+  def test_find
+    ps = %w[she her hers he]
+    m = ACAutomaton.new ps
+    assert_equal(%w[he she], m.find('kasheh').sort)
+  end
+
+  def test_matches
+    ps = %w[bot efg Googlew]
+    m = ACAutomaton.new ps
+    assert(m.matches?('Googlebot'))
+    refute(m.matches?('Google'))
   end
 end
 
@@ -139,26 +167,27 @@ class IPMatcherTest < Minitest::Test
     kw = [k1, k2, k3, k4]
     matcher = IPMatcher.new(kw)
     assert(matcher.matches?('192.168.1.1'))
+    refute(matcher.matches?('192.168.2.1'))
     assert(matcher.matches?('255.255.0.255'))
-    assert(!matcher.matches?('255.255.255.0'))
-    assert(!matcher.matches?('210.156.120.95'))
+    refute(matcher.matches?('255.255.255.0'))
+    refute(matcher.matches?('210.156.120.95'))
     assert(matcher.matches?('43.90.1.254'))
-    assert(!matcher.matches?('173.104.1.254'))
-    assert(!matcher.matches?('86.180.1.254'))
+    refute(matcher.matches?('173.104.1.254'))
+    refute(matcher.matches?('86.180.1.254'))
   end
 
   def test_that_blank_pattern_does_not_match
     matcher = ACMatcher.new([''])
-    assert(!matcher.matches?('foo'))
+    refute(matcher.matches?('foo'))
   end
 
   def test_nil_pattern_results_to_no_characters_detected
     matcher = IPMatcher.new(nil)
-    assert(!matcher.matches?('192.168.1.0'))
+    refute(matcher.matches?('192.168.1.0'))
   end
 
   def test_nil_text_never_matches
     matcher = IPMatcher.new(['192.168.1.0/24'])
-    assert(!matcher.matches?(nil))
+    refute(matcher.matches?(nil))
   end
 end
